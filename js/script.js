15073ccc9a6ffcae3c180076dab2cace7e4df244
serpstrong <ommuts@gmail.com>
2020-06-14 22:50:23 +0500

    seohelp24

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// let selectList = document.querySelector('.landing_ii__list-item');
+//
+// selectList.onclick = function () {
+//     selectList.classList.add('is-active')
+// };
+//
+// $('landing_ii__list-item').on('click', function() {
+//   $(this).addClass('is-active')
+//     .siblings().removeClass('is-active');
+// })
